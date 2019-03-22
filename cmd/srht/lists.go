f15c96374f196797e02af4d954430177bdb9ff03
Sam Whited <sam@samwhited.com>
2019-03-22 16:01:18 -0500

    cmd/srht: allow listing posts by user or list

--- cmd/srht/lists.go
+++ cmd/srht/lists.go
@@ -27,6 +27,8 @@ func listsCmd(srhtClient sourcehut.Client, env envVars) (*cli.Command, error) {
 		Description: "Manipulate mailing lists.",
 		Commands: []*cli.Command{
 			getListsUserCmd(client),
+			listEmailsCmd(client),
+			listPostsCmd(client),
 			listsVersionCmd(client),
 		},
 		Run: func(c *cli.Command, _ ...string) error {
@@ -46,9 +48,6 @@ func getListsUserCmd(client *lists.Client) *cli.Command {
 			case 0:
 			case 1:
 				username = args[0]
-				if !strings.HasPrefix(username, "~") {
-					username = "~" + username
-				}
 			default:
 				c.Help()
 				return nil
@@ -75,6 +74,60 @@ func listsVersionCmd(client *lists.Client) *cli.Command {
 			}
 			fmt.Println(ver)
 			return nil
+		},
+	}
+}
+
+func listEmailsCmd(client *lists.Client) *cli.Command {
+	return &cli.Command{
+		Usage:       "emails username",
+		Description: "List all emails sent by the given user.",
+		Run: func(c *cli.Command, args ...string) error {
+			username := ""
+			switch len(args) {
+			case 1:
+				username = args[0]
+			default:
+				c.Help()
+				return nil
+			}
+
+			posts, err := client.ListEmails(username)
+			if err != nil {
+				return err
+			}
+			for posts.Next() {
+				// TODO: formatting
+				fmt.Printf("%+v\n", posts.Post())
+			}
+			return posts.Err()
+		},
+	}
+}
+
+func listPostsCmd(client *lists.Client) *cli.Command {
+	return &cli.Command{
+		Usage:       "posts username/listname",
+		Description: "List all emails to the list owned by the given username.",
+		Run: func(c *cli.Command, args ...string) error {
+			parts := make([]string, 2)
+			switch len(args) {
+			case 1:
+				parts = strings.SplitN(args[0], "/", 2)
+			default:
+				c.Help()
+				return nil
+			}
+
+			posts, err := client.ListPosts(parts[0], parts[1])
+			if err != nil {
+				return err
+			}
+			for posts.Next() {
+				// TODO: formatting
+				fmt.Printf("%+v\n", posts.Post())
+			}
+			return posts.Err()
 		},
 	}
 }
